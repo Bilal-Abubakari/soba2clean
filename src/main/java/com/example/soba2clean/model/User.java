e2d7c002fa3ac28ae64e961b68912d2c8f18c892
= <abubakaribilal99@gmail.com>
2025-07-15 19:43:42 +0000

    feat: add authentication endpoints for login and registration with JWT support

--- src/main/java/com/example/soba2clean/model/User.java
+++ src/main/java/com/example/soba2clean/model/User.java
@@ -1,10 +1,8 @@
 package com.example.soba2clean.model;
 
-import com.example.soba2clean.dto.RegisterDto;
+import com.example.soba2clean.dto.authentication.RegisterDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "sobaUser")
@@ -13,23 +11,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "First name is required")
     private String firstName;
 
-    @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @Email(message = "Email is not valid")
     private String email;
 
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    @JsonIgnore
     private String password;
 
-    @Pattern(regexp = "^(?:0\\d{9}|\\+233\\d{9})$",
-             message = "Phone number must be a valid Ghanaian number starting with 0 or +233")
     private String phoneNumber;
 
-    @NotBlank(message = "Address is required")
     private String address;
 
     public Long getId() {
@@ -56,13 +48,20 @@ public class User {
         return address;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setUser(RegisterDto registerDto) {
         firstName = registerDto.getFirstName();
         lastName = registerDto.getLastName();
         email = registerDto.getEmail();
-        password = registerDto.getPassword();
         phoneNumber = registerDto.getPhoneNumber();
         address = registerDto.getAddress();
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 }
